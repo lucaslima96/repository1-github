6d28b908e086b373bc64319d7ce47890652e01dc
João Lucas Lima <96lucaslima@gmail.com>
2022-02-10 23:17:32 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Hospede;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Hospede[] vect = new Hospede[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Quarto: ");
+			int quarto = sc.nextInt();
+			vect[quarto] = new Hospede(nome, email);
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms:");
+		for (int i=0; i<10; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		sc.close();
+
+	}
+}
